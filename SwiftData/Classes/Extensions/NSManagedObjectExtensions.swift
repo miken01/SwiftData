e896d705e7d24bb50d2883141078631ea5361d2d
Neill, Mike <mneill@cardinalsolutions.com>
2016-04-18 14:15:38 -0400

    Added Swift 3.0 Support

---
+++ SwiftData/Classes/Extensions/NSManagedObjectExtensions.swift
@@ -0,0 +1,28 @@
+//
+//  NSManagedObjectExtensions.swift
+//  SwiftData
+//
+//  Created by Mike Neill on 10/26/15.
+//  Copyright © 2015 Mike's App Shop. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public extension NSManagedObject {
+    
+    public class func entityName() -> String? {
+        assertionFailure("NSManagedObject.entityName() must be implemented")
+        return nil
+    }
+    
+    public class func newEntity() -> AnyObject? {
+        
+        if let name = self.entityName() {
+            let entity = NSEntityDescription.insertNewObjectForEntityForName(name, inManagedObjectContext: SwiftDataManager.sharedManager.managedObjectContext)
+            return entity
+        }
+        
+        return nil
+    }
+}
